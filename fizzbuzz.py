126664861d38f9c42321539c265200b9c9c53a97
adityamujumdar <47087525+adityamujumdar@users.noreply.github.com>
2020-05-15 13:06:54 -0700

    Create fizzbuzz.py

---
+++ fizzbuzz.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# This program is a fizzbuzz generator for numbers divisible by 3 and/or 5
+__author__ = "Aditya Mujumdar"
+
+__email__ = "ammujumd@asu.edu"
+
+def fizzbuzz(start, stop, step):
+    stop+=step
+    for i in range(start,stop,step):
+        ret_str = ''
+        if(i%3==0):
+            ret_str+='fizz'
+        if(i%5==0):
+            ret_str+='buzz'
+        if not ret_str:
+            ret_str = i
+        print(ret_str)
+    
+def main():
+    start = input('Enter start position of fizzbuzz: ')
+    stop = input('Enter stop position of fizzbuzz: ')
+    step = input('Enter step size for fizzbuzz: ')
+    
+    fizzbuzz(int(start), int(stop), int(step))
+    
+    print('Program has ended. Have a nice day!')
+    
+    
+main()
